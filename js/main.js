8b0a38b3d605db5171a89f7d4b37d19fc4ded3ec
JamesReginald <jim@bpool.nl>
2020-02-24 18:45:45 +0100

    bux fix

--- js/main.js
+++ js/main.js
@@ -124,7 +124,6 @@ var myBBAChart = new Chart(bba, {
     },
     options: {
         scales: {
-            responsive: true,
             yAxes: [{
                 ticks: {
                     suggestedMax: 10,
@@ -215,20 +214,20 @@ var myChart = new Chart(msc, {
         }
     }
 });
-// Validating Empty Field
-function check_empty() {
-    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-    alert("Fill All Fields !");
-    } else {
-    document.getElementById('form').submit();
-    alert("Form Submitted Successfully...");
-    }
-    }
-    //Function To Display Popup
-    function div_show() {
-    document.getElementById('abc').style.display = "block";
-    }
-    //Function to Hide Popup
-    function div_hide(){
-    document.getElementById('abc').style.display = "none";
-    }
+// // Validating Empty Field
+// function check_empty() {
+//     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+//     alert("Fill All Fields !");
+//     } else {
+//     document.getElementById('form').submit();
+//     alert("Form Submitted Successfully...");
+//     }
+//     }
+//     //Function To Display Popup
+//     function div_show() {
+//     document.getElementById('abc').style.display = "block";
+//     }
+//     //Function to Hide Popup
+//     function div_hide(){
+//     document.getElementById('abc').style.display = "none";
+//     }
